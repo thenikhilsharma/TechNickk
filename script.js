f5675ec275601c658022eb59e86f02a3c5ed1f9c
Nikhil Sharma <nikhilsharmalku@gmail.com>
2024-07-07 04:07:44 +0530

    07.07.2024.c2
    7th June 2024 4:07 AM
    commit 2

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
-const a = 5;
-console.log(a);
+// let a = document.getElementsByClassName("box");
+// let b = document.getElementById("red");
+// a[2].style.backgroundColor = "red";
+// b.style.backgroundColor = "blue";
+
+// let div = document.createElement("div");
+// div.innerHTML = "i have been inserted";
+// div.setAttribute("class", "box");
+// document.querySelector(".container").append(div);
+
+function createCard(title, cName, views, monthsOld, duration){
+    let doc = document.querySelector(".container");
+    let changeTitle = document.getElementById("title");
+    changeTitle.innerHTML = title;
+    let changeName = document.getElementById("cName");
+    changeName.innerHTML = cName;
+    let changeViews = document.getElementById("views");
+    changeViews.innerHTML = views;
+    let changeMonths = document.getElementById("monthsOld");
+    changeMonths.innerHTML = monthsOld;
+    let changeDuration = document.getElementById("duration");
+    changeDuration.innerHTML = duration;
+}
+
+let title = "Installing VS CODE & How Websites Work";
+let cName = "CodeWithHarry";
+let views = "173k";
+let monthsOld = "3 months";
+let duration = "27:51";
+createCard(title, cName, views, monthsOld, duration);
