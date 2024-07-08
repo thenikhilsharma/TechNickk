4c6c3e6a4dda995eb5591b65797ec43606b543c7
Nikhil Sharma <nikhilsharmalku@gmail.com>
2024-07-09 02:16:30 +0530

    09.07.2024.c1
    9th July 2024 2:16AM
    commit 1

--- musicPlayer/script.js
+++ musicPlayer/script.js
@@ -1,1 +1,12 @@
-console.log("running")
+const imageContainer = document.getElementsByClassName("card");
+const hoverButton = document.getElementsByClassName("play");
+
+imageContainer.addEventListener('mouseover', function () {
+    hoverButton.style.opacity = '1';
+    hoverButton.style.pointerEvents = 'auto';
+});
+
+imageContainer.addEventListener('mouseout', function () {
+    hoverButton.style.opacity = '0';
+    hoverButton.style.pointerEvents = 'none';
+});
